c58f782674aaa2fdec6933fabfea48c15c7b2699
Andreas Scherren <andreas@spellfusion.com>
2024-10-26 19:55:50 +0200

    ShowInInspector makes things implicit

--- Runtime/Attributes/ShowInInspector.cs
+++ Runtime/Attributes/ShowInInspector.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
-
 namespace TriInspector
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     [Conditional("UNITY_EDITOR")]
+    [JetBrains.Annotations.MeansImplicitUse]
     public class ShowInInspectorAttribute : Attribute
     {
     }
